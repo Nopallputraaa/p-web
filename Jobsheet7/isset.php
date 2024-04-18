4298c051cf08c0a5c37a18494c5f94c588628d3a
Nopallputraaa <166371877+Nopallputraaa@users.noreply.github.com>
2024-04-18 23:41:00 +0700

    Add files via upload

---
+++ Jobsheet7/isset.php
@@ -0,0 +1,20 @@
+
+<?php
+// Soal 1.1
+$umur;
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa.";
+} else {
+    echo "Anda belum dewasa atau variabel umur tidak ditemukan";
+}
+
+echo "<br><br>";
+// Soal 1.2
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+
+?>
